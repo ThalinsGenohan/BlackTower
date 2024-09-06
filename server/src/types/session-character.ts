2529e76181faafd7f5d7bac31fa5ca58287bbaa7
Thalins Genohan <thalinsgenohan@gmail.com>
2024-09-06 13:58:06 -0400

    Update session-character.ts

--- server/src/types/session-character.ts
+++ server/src/types/session-character.ts
@@ -1,4 +1,4 @@
-import { Character, JobClass } from 'types/character';
+import { Character, JobClass, PronounList } from 'types/character';
 
 export class SessionCharacter {
     static baseHP = 10;
@@ -16,46 +16,54 @@ export class SessionCharacter {
 
     full: Character;
 
-    get maxHP() {
+    get maxHP(): number {
         return SessionCharacter.baseHP +
             this.full.specialtyClass.hp +
             (this.equippedClass == null ? 0 : this.equippedClass.hp);
     }
-    get maxMP() {
+    get maxMP(): number {
         return SessionCharacter.baseMP +
             this.full.specialtyClass.mp +
             (this.equippedClass == null ? 0 : this.equippedClass.mp);
     }
-    get strength() {
+    get strength(): number {
         return SessionCharacter.baseStrength +
             this.full.specialtyClass.strength +
             (this.equippedClass == null ? 0 : this.equippedClass.strength);
     }
-    get magic() {
+    get magic(): number {
         return SessionCharacter.baseMagic +
             this.full.specialtyClass.magic +
             (this.equippedClass == null ? 0 : this.equippedClass.magic);
     }
-    get defense() {
+    get defense(): number {
         return SessionCharacter.baseDefense +
             this.full.specialtyClass.defense +
             (this.equippedClass == null ? 0 : this.equippedClass.defense);
     }
-    get resistance() {
+    get resistance(): number {
         return SessionCharacter.baseResistance +
             this.full.specialtyClass.resistance +
             (this.equippedClass == null ? 0 : this.equippedClass.resistance);
     }
 
-    get name() { return this.full.name; }
-    get pronouns() { return this.full.pronouns; }
-    get species() { return this.full.species; }
-    get player() { return this.full.player; }
-    get image() { return this.full.image; }
-    get specialtyClass() { return this.full.specialtyClass; }
+    get name(): string { return this.full.name; }
+    get pronouns(): PronounList { return this.full.pronouns; }
+    get species(): string { return this.full.species; }
+    get player(): string { return this.full.player; }
+    get image(): string { return this.full.image; }
+    get specialtyClass(): JobClass { return this.full.specialtyClass; }
 
     currentHP: number;
     currentMP: number;
     equippedClass: JobClass | null = null;
     buffs: { [id: string]: number } = {};
+
+    toJSON() {
+        return { ...this.full, ...this };
+    }
+}
+
+export class Buff {
+
 }
